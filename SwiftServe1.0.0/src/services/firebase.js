8966eac8a77d06fde84a8a2e4e69afc2cc88cce4
Cepheux <E0969072@U.NUS.EDU>
2024-10-10 23:33:44 +0800

    Create firebase.js
    Firebase and Firestore, init,
    
    TO DO:
    .env
    testing
    functions
    CRUD

---
+++ SwiftServe1.0.0/src/services/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
+
+
+/* Developer Notes:
+new functions declared not connected yet
+set up env for team
+testing needs to be set up too
+*/
