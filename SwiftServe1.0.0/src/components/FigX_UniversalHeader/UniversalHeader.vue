d3f66576546cdd39232450db2616cedaa0f976fc
Yeo-Zuosheng <yeo.fmy@gmail.com>
2024-10-11 21:18:06 +0800

    Added UniversalHeader

--- SwiftServe1.0.0/src/components/FigX_UniversalHeader/UniversalHeader.vue
+++ SwiftServe1.0.0/src/components/FigX_UniversalHeader/UniversalHeader.vue
@@ -1,11 +1,82 @@
 <template>
-    <img src="/SwiftServeLogo.png" alt="SwiftServeLogo" class="SSLogo" />
+    <div class="headerContainer">
+        <div id="logo">
+            <img src="/SwiftServeLogo.png" alt="SwiftServeLogo" class="SSLogo" />
+            <h1> SwiftServe </h1>
+        </div>
+
+        <div class="welcomeCust">
+            <h2> Welcome Spencer TBC </h2>
+        </div>
+
+        <img src="/dashboardButton.png" alt="DashboardButton" class="headerButtons" id="dButton" @click="CDashClick"/>
+        <img src="/settingsButton.png" alt="SettingsButton" class="headerButtons" id="sButton" @click="SettingsClick"/>
+        <img src="/logoutButton.png" alt="LogoutButton" class="headerButtons" id="lButton" @click="LogoutClick"/>
+    
+    </div>
 </template>
 
 <script>
+export default {
+  methods: {
+    CDashClick() {
+      //logout from firebase. NOT DONE YET.
+      this.$router.push('/custD');
+    },
 
+    //SettingsClick. NOT DONE YET.
+    
+    LogoutClick() {
+      this.$router.push('/');
+    }
+  }
+}
 </script>
 
 <style scoped>
+.headerContainer {
+    display: flex;
+    align-items: center;
+}
+
+#logo {
+    display: flex;
+    align-items: center;
+}
+
+img {
+    width: 7vw;
+}
+
+h1 {
+  color: #00ADB5;
+  font-weight: bold;
+  font-family: 'Mada';
+  font-size: 5vh;
+}
+
+.welcomeCust {
+    position: absolute;
+    left: 40vw;
+}
+
+.headerButtons {
+    width: 4vw;
+}
+
+#dButton {
+    position: absolute;
+    left: 83vw;
+}
+
+#sButton {
+    position: absolute;
+    left: 89vw;
+}
+
+#lButton {
+    position: absolute;
+    left: 95vw;
+}
 
 </style>
