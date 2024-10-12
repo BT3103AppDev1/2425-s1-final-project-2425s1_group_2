5d09e2ecd8ec00cdf8872b369c12e4a95e00e715
Yeo-Zuosheng <yeo.fmy@gmail.com>
2024-10-12 23:19:27 +0800

    app.vue improved

--- SwiftServe1.0.0/src/App.vue
+++ SwiftServe1.0.0/src/App.vue
@@ -1,27 +1,9 @@
-<<<<<<< HEAD
 <template>
   <header>
     <!--
     <div class="nav">
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
-=======
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/hawker">Hawker</RouterLink>
-      </nav>
->>>>>>> cedric-09/10-HawkerCentrePage
     </div>
     -->
   </header>
@@ -38,6 +20,5 @@ export default {
 
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Inria+Sans:wght@400;700&display=swap');
 
 </style>
