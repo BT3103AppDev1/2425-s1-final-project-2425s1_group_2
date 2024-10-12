bb20dba232740f07989fae4799dcde10e0b6ea3f
Yeo-Zuosheng <yeo.fmy@gmail.com>
2024-10-12 21:55:40 +0800

    edited customer dashboard css

--- SwiftServe1.0.0/src/views/Fig7_CustDashView.vue
+++ SwiftServe1.0.0/src/views/Fig7_CustDashView.vue
@@ -1,17 +1,20 @@
 <template>
 <HeaderScreen/>
 <CustDScreen/>
+<CustHistoryScreen/>
 </template>
 
 <script>
 import HeaderScreen from "@/components/FigX_UniversalHeader/UniversalHeader.vue";
 import CustDScreen from "@/components/Fig7_CustDashboardPage/CustOrders.vue";
+import CustHistoryScreen from "@/components/Fig7_CustDashboardPage/CustOrderHistory.vue";
 
 export default {
   name: 'CustDashboardPage',
   components: {
     HeaderScreen,
-    CustDScreen
+    CustDScreen,
+    CustHistoryScreen
   },
 
   data() {
