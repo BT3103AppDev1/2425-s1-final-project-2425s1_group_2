5b1d65a3485e0b9db308214860220edd33553082
Cepheux <E0969072@U.NUS.EDU>
2024-10-30 23:59:29 +0800

    resetPassword Page as per fig 19

--- SwiftServe1.0.0/src/router/index.js
+++ SwiftServe1.0.0/src/router/index.js
@@ -154,6 +154,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/Fig18_MerchantSalesDashboardView.vue')
+    },
+    {
+      path: '/resetPassword',
+      name: 'resetPasswordPage',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Fig19_ResetPasswordView.vue')
     }
   ]
 })
---
+++ SwiftServe1.0.0/src/views/Fig19_ResetPasswordView.vue
@@ -0,0 +1,19 @@
+<template>
+  <LoginHeader />
+  <LoginScreen />
+</template>
+
+<script>
+import LoginHeader from '@/components/FigX_UniversalHeader/LoginHeader.vue'
+import ResetPassword from '@/components/Fig19_ResetPasswordPage/ResetPassword.vue'
+
+export default {
+  name: 'LoginView',
+  components: {
+    LoginHeader,
+    ResetPassword
+  },
+
+  data() {}
+}
+</script>
